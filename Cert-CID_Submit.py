49fa98ec537a314c472181dbc27f5c2a671a5727
AlexWen <alex.wen@canonical.com>
2018-02-02 14:30:15 +0800

    Coding now...

--- Cert-CID_Submit.py
+++ Cert-CID_Submit.py
@@ -1,5 +1,12 @@
-#!/usr/bin/python
-
+#!/usr/bin/python 
 #alex.wen@canonical.com
 
+import sys
+import requests
 
+if len(sys.argv) != 2:
+    print "Bye"
+    sys.exit()
+
+r = requests.get("https://goo.gl/J1UuJJ")
+print r.text
